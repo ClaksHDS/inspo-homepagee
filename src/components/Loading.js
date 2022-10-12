69c0950f5284bf6c66cc11fd8a5ec23b119cec33
[claks HDS] <you@example.com>
2022-10-12 16:39:39 +0100

    Loading component + style

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <aside className='loading-container'>
+      <div className='loading'></div>
+    </aside>
+  );
+};
+
+export default Loading;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 // to import and export all components directly from the components folder
+
+import Loading from "./Loading";
+
+export { Loading };
