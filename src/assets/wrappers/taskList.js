e8b9b5e95ca86864eebc009b57cedeb949b15a48
[claks HDS] <you@example.com>
2022-10-19 13:07:51 +0100

    Styled TaskList

--- src/assets/wrappers/taskList.js
+++ src/assets/wrappers/taskList.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  .tasks-list {
+    background: var(--backgroundColor);
+    border-radius: var(--borderRadius);
+    width: 90vw;
+    max-width: var(--fixedWidth);
+    box-shadow: var(--shadow-2);
+    padding: 2rem 2rem;
+    margin: 3rem auto;
+  }
+`;
 
 export default Wrapper;
